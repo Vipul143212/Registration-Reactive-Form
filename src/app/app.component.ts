a7e7f3f38c4c55cf5eaa7ed4470eb7d576c5cdcf
Vipul143212 <vipulu563@gmail.com>
2023-09-01 10:12:07 +0530

    Implement Registration using reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RegistrationFormTemplateDriven';
+  gender = 'male'; //default value
+  selectedBranches:Array<number> = [];
+  
+  RegisterStudent(studentForm: NgForm): void {   
+    console.log(studentForm.value);
+    console.log(studentForm.valid);
+    console.log(studentForm.value.firstName);
+    const fName:string = studentForm.controls['firstName'].value;
+    console.log(fName);
+    console.log("touched form");
+    console.log(studentForm.touched);
+    console.log(this.selectedBranches);     
+  }
 }
